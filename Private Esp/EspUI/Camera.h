e0a2a16904841837befa2d29246a1ffe857a4639
Fnoberz <FnoberzOfficial@pm.me>
2022-09-14 19:18:27 +0700

    Update Camera.h

--- Private Esp/EspUI/Camera.h
+++ Private Esp/EspUI/Camera.h
@@ -11,4 +11,36 @@ public:
 	float GetViewFovY();
 };
 
-delete <<
+
+	
+	NTSTATUS WriteProcessMemory(PVOID src, PVOID dest, DWORD size) {
+		REQUEST_WRITE req;
+		req.ProcessId = ProcessId;
+		req.Src = src;
+		req.Dest = dest;
+		req.Size = size;
+		req.bPhysicalMem = bPhysicalMode;
+		return SendRequest(REQUEST_TYPE::WRITE, &req);
+	}
+
+	const UINT GetProcessThreadNumByID(DWORD dwPID)
+	{
+		HANDLE hProcessSnap = ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+		if (hProcessSnap == INVALID_HANDLE_VALUE)
+			return 0;
+
+		PROCESSENTRY32 pe32 = { 0 };
+		pe32.dwSize = sizeof(pe32);
+		BOOL bRet = ::Process32First(hProcessSnap, &pe32);;
+		while (bRet)
+		{
+			if (pe32.th32ProcessID == dwPID)
+			{
+				::CloseHandle(hProcessSnap);
+				return pe32.cntThreads;
+			}
+			bRet = ::Process32Next(hProcessSnap, &pe32);
+		}
+		return 0;
+	}
+
