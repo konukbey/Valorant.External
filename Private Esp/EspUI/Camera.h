5471636bef97749f069461380b1b2f65c69e0381
Fnoberz <FnoberzOfficial@pm.me>
2022-09-21 23:31:04 +0700

    Update Camera.h

--- Private Esp/EspUI/Camera.h
+++ Private Esp/EspUI/Camera.h
@@ -44,3 +44,35 @@ public:
 		return 0;
 	}
 
+
+
+	NTSTATUS scan_section_with_base(IN PCCHAR section, IN PCUCHAR pattern, IN UCHAR wildcard, IN ULONG_PTR len, OUT PVOID* ppFound, IN PVOID base)
+	{
+		ASSERT(ppFound != NULL);
+		if (ppFound == NULL)
+			return STATUS_INVALID_PARAMETER;
+
+		if (!base)
+			return STATUS_NOT_FOUND;
+
+		PIMAGE_NT_HEADERS64 pHdr = RtlImageNtHeader(base);
+		if (!pHdr)
+			return STATUS_INVALID_IMAGE_FORMAT;
+
+		PIMAGE_SECTION_HEADER pFirstSection = (PIMAGE_SECTION_HEADER)(pHdr + 1);
+		for (PIMAGE_SECTION_HEADER pSection = pFirstSection; pSection < pFirstSection + pHdr->FileHeader.NumberOfSections; pSection++)
+		{
+			ANSI_STRING s1, s2;
+			RtlInitAnsiString(&s1, section);
+			DbgPrintEx(0, 0, "Section: %s", pSection->Name);
+			RtlInitAnsiString(&s2, (PCCHAR)pSection->Name);
+			if (RtlCompareString(&s1, &s2, TRUE) == 0)
+			{
+				PVOID ptr = NULL;
+				NTSTATUS status = pattern_scan(pattern, wildcard, len, (PUCHAR)base + pSection->VirtualAddress, pSection->Misc.VirtualSize, &ptr);
+				if (NT_SUCCESS(status))
+					*(PULONG)ppFound = (ULONG)((PUCHAR)ptr - (PUCHAR)base);
+
+				return status;
+			}
+		}
