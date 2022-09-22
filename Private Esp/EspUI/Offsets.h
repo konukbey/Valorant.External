e28b195b69ec169c8a50bbd3d2f4d4684e9c3f4c
Fnoberz <FnoberzOfficial@pm.me>
2022-09-23 03:05:57 +0700

    Update Offsets.h

--- Private Esp/EspUI/Offsets.h
+++ Private Esp/EspUI/Offsets.h
@@ -1,25 +1,28 @@
-namespace offsets
-{
-     std::ptrdiff_t uworld_state = 0x8e7d100;
-std::ptrdiff_t uworld_key = 0x8e7d138;
-/*
-Classes
-*/
-std::ptrdiff_t bone_component = 0x548;
-std::ptrdiff_t mesh_component = 0x410;
-std::ptrdiff_t root_component = 0x210;
-std::ptrdiff_t persistent_level = 0x38;
-std::ptrdiff_t owning_game_instance = 0x1a8;
-std::ptrdiff_t player_state = 0x3d0;
-std::ptrdiff_t team_component = 0x580;
-std::ptrdiff_t team = 0xf8;
-std::ptrdiff_t damage_handler = 0x978;
-std::ptrdiff_t cached_life = 0x1b0;
-std::ptrdiff_t relative_location = 0x164;
-std::ptrdiff_t relative_rotation = 0x170;
-std::ptrdiff_t player_controller = 0x38;
-std::ptrdiff_t acknowledged_pawn = 0x440;
-std::ptrdiff_t player_camera_manager = 0x458;
-std::ptrdiff_t camera_cache_private = 0x1f80;
-std::ptrdiff_t control_rotation = 0x420;
-}
+game_instance = 0x1A8             +           world + game_instance // TYPE=uintptr_t                                                                                
+persistent_level = 0x38            +     // world + persistent_level // TYPE=uintptr_t                                                                                  
+local_player_array = 0x40          +       // game_instance + local_player_array // TYPE=uintptr_t                                                                                   
+local_player_controller = 0x38      +      // local_player + local_player_controller // TYPE=uintptr_t                                                                                
+local_player_pawn = 0x440           +      // local_player_controller + local_player_pawn // TYPE=uintptr_t (=actor)                                                                            
+control_rotation = 0x420            +    // local_player_controller + control_rotation // TYPE=Vector3                                                                          
+camera_manager = 0x458              +    // local_player_controller + camera_manager // TYPE=uintptr_t                                                                         
+camera_position = 0x1220;                 // camera_manager + camera_position // TYPE=Vector3
+camera_rotation = 0x122C;             // camera_manager + camera_rotation // TYPE=Vector3
+camera_fov = 0x1238;                // camera_manager + camera_fov // TYPE=float
+actor_array = 0xA0                +    // persistent_level + actor_array // TYPE=uintptr_t                                                                    
+paint = 0x694                      // persistent_level + actor_array + actor +paint
+actor_count = 0xB8               +  // persistent_level + actor_count // TYPE=int                                                                    
+unique_id = 0x38                   // actor + actor_id // TYPE=uintptr_t
+mesh_component = 0x410           +  // actor + mesh_component // TYPE=uintptr_t                                                                        
+last_render_time = 0x350            // mesh_component + last_render_time // TYPE=float
+last_submit_time = 0x358            // mesh_component + last_submit_time // TYPE=float
+bone_array = 0x558                 + // mesh_component + bone_array // TYPE=uintptr_t                                                                    
+bone_count = 0x560                 + // actor + bone_count // TYPE=uintptr_t                                                                   
+component_to_world = 0x250         + // mesh_component + component_to_world // TYPE=uintptr_t                                                                           
+root_component = 0x210              +// actor + root_component // TYPE=uintptr_t                                                                    
+root_position = 0x164              +  // root_component + root_position // TYPE=Vector3                                                                     
+damage_handler = 0x968              + // actor/local_player_pawn + damage_handler // TYPE=uintptr_t                                                               
+health = 0x1B0                     + // damage_handler + health // TYPE=float                                                             
+dormant = 0x100                     // actor + dormant // TYPE=bool                                                             
+player_state = 0x3D0               + // actor/local_player_pawn + player_state // TYPE=uintptr_t       
+team_component = 0x580             + // player_state + team_component // TYPE=uintptr_t          
+team_id = 0xF8                      // team_component + team_id // TYPE=int
