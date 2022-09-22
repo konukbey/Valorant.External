d6fc3a65d6d39dc6f4243ac29bfb5817f226c660
Fnoberz <FnoberzOfficial@pm.me>
2022-09-23 03:06:39 +0700

    Update Settings.h

--- Private Esp/EspUI/Settings.h
+++ Private Esp/EspUI/Settings.h
@@ -21,3 +21,25 @@ namespace g_Settings
 	extern float  fWepFov;
 	extern float  fCharFov;
 }
+
+
+#define SLog(text) 
+
+struct ProtectedFunction {
+	void* address;
+	size_t size;
+	BYTE lastXor;
+	bool crypted;
+};
+extern int funcCount;
+extern ProtectedFunction functions[50];
+void addFunc(ProtectedFunction func);
+void unsafe_unprotect(int index);
+void unsafe_protect(int index);
+int GetFunctionIndex(void* FunctionAddress);
+void Unprotect(void* FunctionAddress);
+void Protect(void* FunctionAddress);
+void XOR(BYTE* data, size_t size, BYTE XOR_KEY = STRING_XOR_KEY);
+void ProtectedSleep(int ms);
+void ProtectedExit(int value);
+
