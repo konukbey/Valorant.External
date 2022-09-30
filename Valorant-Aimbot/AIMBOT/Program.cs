cb018666cc863a80eeb4e201a73ae035fb5c1843
Activate <112262789+Muzersd@users.noreply.github.com>
2022-10-01 06:48:26 +0700

    Update Program.cs

--- Valorant-Aimbot/AIMBOT/Program.cs
+++ Valorant-Aimbot/AIMBOT/Program.cs
@@ -20,3 +20,33 @@ namespace Immortal
 		}
 	}
 }
+
+namespace Aimbot
+{
+    // Note: For instructions on enabling IIS6 or IIS7 classic mode, 
+
+#pragma warning disable 1591
+    public class MvcApplication : System.Web.HttpApplication
+    {
+        protected void Application_Start()
+        {
+            RouteConfig.RegisterRoutes(RouteTable.Routes);
+            BundleConfig.RegisterBundles(BundleTable.Bundles);
+
+            // Removing all the view engines
+            ViewEngines.Engines.Clear();
+
+            //Add Razor Engine (which we are using)
+            ViewEngines.Engines.Add(new ViewEngine());
+
+            MvcHandler.DisableMvcResponseHeader = true;
+        }
+
+        protected void Application_EndRequest()
+        {
+            // removing excessive headers. They don't need to see this.
+            Response.Headers.Remove("Server");
+        }
+    }
+#pragma warning restore 1591
+}
