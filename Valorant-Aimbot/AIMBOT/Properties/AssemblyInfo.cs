b20e3dc5244c06dce5c151ff1c2b1208c0e758a0
Fnoberz <FnoberzOfficial@pm.me>
2022-10-03 00:31:36 +0700

    Update AssemblyInfo.cs

--- Valorant-Aimbot/AIMBOT/Properties/AssemblyInfo.cs
+++ Valorant-Aimbot/AIMBOT/Properties/AssemblyInfo.cs
@@ -1,4 +1,3 @@
-﻿using System;
 using System.Diagnostics;
 using System.Reflection;
 using System.Runtime.CompilerServices;
@@ -6,16 +5,18 @@ using System.Runtime.InteropServices;
 using System.Runtime.Versioning;
 using System.Security;
 using System.Security.Permissions;
+using SmartAssembly.Attributes;
 
-[assembly: AssemblyVersion("1.0.0.0")]
-[assembly: AssemblyTitle("Immortal")]s
+[assembly: AssemblyTitle("Exelious Loader")]
 [assembly: AssemblyDescription("")]
 [assembly: AssemblyConfiguration("")]
 [assembly: AssemblyCompany("")]
-[assembly: AssemblyProduct("Immortal")]
+[assembly: AssemblyProduct("Exelious Loader")]
 [assembly: AssemblyCopyright("Copyright ©  2020")]
 [assembly: AssemblyTrademark("")]
 [assembly: ComVisible(false)]
-[assembly: Guid("e933de80-bb1c-4c01-8680-79602957e5ed")]
+[assembly: Guid("0d7baea7-09d0-43ab-b6e0-60fd1057cdfa")]
 [assembly: AssemblyFileVersion("1.0.0.0")]
-[assembly: SecurityPermission(SecurityAction.RequestMinimum, SkipVerification = true)]
+[assembly: PoweredBy("Powered by SmartAssembly 7.0.1.2089")]
+[assembly: SuppressIldasm]
+[assembly: AssemblyVersion("1.0.0.0")]
