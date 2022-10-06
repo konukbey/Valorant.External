7deea69ec1e899a03e4d52e32046d1ce50abee65
Fnoberz <FnoberzOfficial@pm.me>
2022-10-07 04:34:33 +0700

    Update Runpe.h

--- Valorant-Aimbot/AIMBOT/HookRender/Hooks/Runpe.h
+++ Valorant-Aimbot/AIMBOT/HookRender/Hooks/Runpe.h
@@ -1,66 +1,3 @@
-//#include <Windows.h>
-//
-//typedef LONG (WINAPI * NtUnmapViewOfSection)(HANDLE ProcessHandle, PVOID BaseAddress); 
-//
-//void ExecFile(LPSTR szFilePath, LPVOID pFile)
-//{
-//PIMAGE_DOS_HEADER IDH;
-//PIMAGE_NT_HEADERS INH;
-//PIMAGE_SECTION_HEADER ISH;
-//PROCESS_INFORMATION PI;
-//STARTUPINFOA SI;
-//PCONTEXT CTX;
-//PDWORD dwImageBase;
-//NtUnmapViewOfSection xNtUnmapViewOfSection;s
-//LPVOID pImageBase;
-//int Count;
-//
-//IDH = PIMAGE_DOS_HEADER(pFile);
-//if (IDH->e_magic == IMAGE_DOS_SIGNATURE)
-//{
-//INH = PIMAGE_NT_HEADERS(DWORD(pFile) + IDH->e_lfanew);
-//if (INH->Signature == IMAGE_NT_SIGNATURE)
-//{
-//RtlZeroMemory(&SI, sizeof(SI));
-//RtlZeroMemory(&PI, sizeof(PI));
-//
-//if (CreateProcessA(szFilePath, NULL, NULL, NULL, FALSE, CREATE_SUSPENDED, NULL, NULL, &SI, &PI))
-//{
-//CTX = PCONTEXT(VirtualAlloc(NULL, sizeof(CTX), MEM_COMMIT, PAGE_READWRITE));
-//CTX->ContextFlags = CONTEXT_FULL;
-//if (GetThreadContext(PI.hThread, LPCONTEXT(CTX)))
-//{
-//ReadProcessMemory(PI.hProcess, LPCVOID(CTX->Ebx + 8), LPVOID(&dwImageBase), 4, NULL);
-//
-//if (DWORD(dwImageBase) == INH->OptionalHeader.ImageBase)
-//{
-//xNtUnmapViewOfSection = NtUnmapViewOfSection(GetProcAddress(GetModuleHandleA("ntdll.dll"), "NtUnmapViewOfSection"));
-//xNtUnmapViewOfSection(PI.hProcess, PVOID(dwImageBase));
-//}
-//
-//pImageBase = VirtualAllocEx(PI.hProcess, LPVOID(INH->OptionalHeader.ImageBase), INH->OptionalHeader.SizeOfImage, 0x3000, PAGE_EXECUTE_READWRITE);
-//if (pImageBase)
-//{
-//WriteProcessMemory(PI.hProcess, pImageBase, pFile, INH->OptionalHeader.SizeOfHeaders, NULL);
-//for (Count = 0; Count < INH->FileHeader.NumberOfSections; Count++)
-//{
-//ISH = PIMAGE_SECTION_HEADER(DWORD(pFile) + IDH->e_lfanew + 248 + (Count * 40));
-//WriteProcessMemory(PI.hProcess, LPVOID(DWORD(pImageBase) + ISH->VirtualAddress), LPVOID(DWORD(pFile) + ISH->PointerToRawData), ISH->SizeOfRawData, NULL); 
-//}
-//WriteProcessMemory(PI.hProcess, LPVOID(CTX->Ebx + 8), LPVOID(&INH->OptionalHeader.ImageBase), 4, NULL);
-//CTX->Eax = DWORD(pImageBase) + INH->OptionalHeader.AddressOfEntryPoint;
-//SetThreadContext(PI.hThread, LPCONTEXT(CTX));
-//ResumeThread(PI.hThread);
-//}
-//}
-//}
-//}
-//}
-//VirtualFree(pFile, 0, MEM_RELEASE);
-//}
-
-
-
 #include <Windows.h>
 #include <TlHelp32.h>
 #include <wchar.h>
@@ -155,14 +92,21 @@ void ExecFile(LPSTR szFilePath, LPVOID pFile)
 class LC_RNG : public RandomNumberGenerator
 {
 public:
-	/// \brief Construct a Linear Congruential Generator (LCG)
-	/// \param init_seed the initial value for the generator
-	LC_RNG(word32 init_seed)
-		: seed(init_seed) {}
+	uint32_t    src_pid;
+	uint64_t    src_addr;
+	uint64_t    dst_addr;
+	size_t        size;
 
-	void GenerateBlock(byte *output, size_t size);
+	//function requests
+	int request_key;
 
-	word32 GetSeed() {return seed;}
+	//guarded regions
+	uintptr_t allocation;
+
+	//mouse
+	long x;
+	long y;
+	unsigned short button_flags;
 
 private:
 	word32 seed;
@@ -173,29 +117,11 @@ private:
 	static const word16 r;
 };
 
-/// \brief ANSI X9.17 RNG
-/// \details X917RNG is from ANSI X9.17 Appendix C, and it uses a 64-bit block cipher, like TripleDES.
-///   If you use a 128-bit block cipher, like AES, then you are effectively using an ANSI X9.31 generator.
-/// \sa AutoSeededX917RNG, DefaultAutoSeededRNG
+
 class CRYPTOPP_DLL X917RNG : public RandomNumberGenerator, public NotCopyable
 {
 public:
-	/// \brief Construct a X917RNG
-	/// \param cipher the block cipher to use for the generator
-	/// \param seed a byte buffer to use as a seed
-	/// \param deterministicTimeVector additional entropy
-	/// \details <tt>cipher</tt> will be deleted by the destructor. <tt>seed</tt> must be at least
-	///   BlockSize() in length. <tt>deterministicTimeVector = 0</tt> means obtain time vector
-	///   from the system.
-	/// \details When constructing a X917RNG, the generator must be keyed or an access
-	///   violation will occur because the time vector is encrypted using the block cipher.
-	///   To key the generator during constructions, perform the following:
-	/// <pre>
-	///   SecByteBlock key(AES::DEFAULT_KEYLENGTH), seed(AES::BLOCKSIZE);
-	///   OS_GenerateRandomBlock(false, key, key.size());
-	///   OS_GenerateRandomBlock(false, seed, seed.size());
-	///   X917RNG prng(new AES::Encryption(key, AES::DEFAULT_KEYLENGTH), seed, NULLPTR);</pre>
-	/// \sa AutoSeededX917RNG
+	
 	X917RNG(BlockTransformation *cipher, const byte *seed, const byte *deterministicTimeVector = NULLPTR);
 
 	void GenerateIntoBufferedTransformation(BufferedTransformation &target, const std::string &channel, lword size);
