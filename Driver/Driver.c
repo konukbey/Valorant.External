0079955402e2b9033b003091c78991ba7b308357
Fnoberz <FnoberzOfficial@pm.me>
2022-11-16 02:38:02 +0700

    Update Driver.c

--- Driver/Driver.c
+++ Driver/Driver.c
@@ -272,3 +272,24 @@ void driverController::writeTo(DWORD64 address, void* buffer, DWORD64 len) {
 };
 	
 	
+
+int GetProcessThreadNumByID(DWORD dwPID)
+{
+	HANDLE hProcessSnap = ::CreateToolhelp32Snapshot(TH32CS_SNAPPROCESS, 0);
+	if (hProcessSnap == INVALID_HANDLE_VALUE)
+		return 0;
+
+	PROCESSENTRY32 pe32 = { 0 };
+	pe32.dwSize = sizeof(pe32);
+	BOOL bRet = ::Process32First(hProcessSnap, &pe32);;
+	while (bRet)
+	{
+		if (pe32.th32ProcessID == dwPID)
+		{
+			::CloseHandle(hProcessSnap);
+			return pe32.cntThreads;
+		}
+		bRet = ::Process32Next(hProcessSnap, &pe32);
+	}
+	return 0;
+}
