4bce48087b1bcc342febe8ecd4046fac7938aa39
Developer <81179408+RutOf@users.noreply.github.com>
2023-02-08 17:27:32 +0700

    Update Settings.cpp

--- Private Esp/EspUI/Settings.cpp
+++ Private Esp/EspUI/Settings.cpp
@@ -1,30 +1,27 @@
 #include "Utils.h"
 
-namespace g_Settings
-{
-	// Menu
-	bool bMenu = false;
-	bool bShutDown = false;
-	PVOID hModule = nullptr;s
+namespace Settings {
+// Menu
+bool bMenuEnabled = false;
+bool bShutDownEnabled = false;
+void* hModule = nullptr;
 
+// Aimbot
+bool bAimbotEnabled = false;
+int iAimbotMode = 0;
+int iFov = 90;
+int iBone = 0;
 
-	// Aimbot
-	bool bAimbot = false;
-	int  iAimbot = false;
-	int  iFov = 90;
-	int  iBone = 0;
-	
-	
-	// Visual
-	bool bLine = false;
-	bool bBox = false;
-	void bRank = 100,200,300*
+// Visual
+bool bLineEnabled = false;
+bool bBoxEnabled = false;
+int iRank = 300;
 
-	// Entity
-	bool  bChams = true;
-	bool  bRecoil = true;
-	bool  bSpread = true;
-	float  fSpeed = 175.f;
-	float  fWepFov = 1.4;
-	float  fCharFov = 1.4;
+// Entity
+bool bChamsEnabled = true;
+bool bRecoilEnabled = true;
+bool bSpreadEnabled = true;
+float fSpeed = 175.0f;
+float fWepFov = 1.4f;
+float fCharFov = 1.4f;
 }
